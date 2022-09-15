4986b078d5554aa472c87e851cceff8d9e487947
tarzanzito <tarzanzito@gmail.com>
2022-09-15 15:59:34 +0100

    V1

---
+++ MusicCollectionList/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+ConsoleApp2.Class1 class1 = new ConsoleApp2.Class1();
+class1.LoadDirectory(@"\\NAS-QNAP\music\_COLLECTION");
+//class1.LoadDirectory(@"D:\_COLLECTION");
