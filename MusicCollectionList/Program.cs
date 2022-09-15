7207fac1a2ab2d35b87909d6f5ae4dbd023669c4
tarzanzito <tarzanzito@gmail.com>
2022-09-15 18:31:50 +0100

    V2

--- MusicCollectionList/Program.cs
+++ MusicCollectionList/Program.cs
@@ -1,8 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace MusicCollection
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
 
 
 
-ConsoleApp2.Class1 class1 = new ConsoleApp2.Class1();
-class1.LoadDirectory(@"\\NAS-QNAP\music\_COLLECTION");
-//class1.LoadDirectory(@"D:\_COLLECTION");
+            MusicCollection.Extractor extractor = new ();
+            extractor.LoadDirectory(@"\\NAS-QNAP\music\_COLLECTION");
+            //class1.LoadDirectory(@"D:\_COLLECTION");
+        }
+    }
+}
